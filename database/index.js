8265ab43e2b8ad2081c1f80711ed35f8637699b2
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-02 16:47:42 +0300

    New db classes added

--- database/index.js
+++ database/index.js
@@ -2,10 +2,16 @@ const User = require('./User')
 const Author = require('./Author')
 const Text = require('./Text')
 const Word = require('./Word')
+const Settings = require('./Settings')
+const Tag = require('./Tag')
+const Train = require('./Train')
 
 module.exports = {
     User,
     Author,
     Text,
-    Word
+    Word,
+    Settings,
+    Tag,
+    Train
 }
