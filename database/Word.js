e7522f8cecc7037ebcefe799b554a2e38a7e6ebd
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-03 15:47:45 +0300

    Query erros solved

--- database/Word.js
+++ database/Word.js
@@ -5,14 +5,14 @@ add = async (data) => {
         var conneciton = pool.getPool()
 
         let query = {
-            text: 'INSERT INTO word(textid,word) VALUES($1, $2)',
+            text: 'INSERT INTO word(textid,word) VALUES($1, $2) RETURNING wordid',
             values: [data.textID, data.word]
         }
 
         text = await conneciton.query(query)
 
         if (text.rowCount != 0) {
-            return { status: true, message: 'Text successfully added' }
+            return { status: true, message: 'Word successfully added', id: text.rows[0].wordid }
         } else {
             return { status: false, message: 'Query error, please try again' }
         }
