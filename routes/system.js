c425002901f000435e1bf89fc89cce3520b361ce
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-06 19:52:39 +0300

    System routes added

--- routes/system.js
+++ routes/system.js
@@ -2,29 +2,146 @@ const router = require('express').Router()
 const DB = require('../database/index')
 const verify = require('../middleware/verify')
 
-router.get('/getUsers', async (req, res) => {})
-router.get('/getAuthors', async (req, res) => {})
-router.get('/getSettings', async (req, res) => {})
-router.get('/getTagTypes', async (req, res) => {})
-router.get('/getTrains', async (req, res) => {})
-router.get('/getTrainExcludes', async (req, res) => {})
+router.get('/getUsers', async (req, res) => {
+    try {
+        data = {}
+        page = parseInt(req.query.page || 0)
+        data.offset = page * 15
+        result = await DB.User.list(data)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.get('/getAuthors', async (req, res) => {
+    try {
+        data = {}
+        page = parseInt(req.query.page || 0)
+        data.offset = page * 15
+        result = await DB.Author.list(data)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
 
-router.post('/addAuthor', async (req, res) => {})
-router.post('/deleteAuthor', async (req, res) => {})
-router.post('/updateAuthor', async (req, res) => {})
+router.get('/getSettings', async (req, res) => {
+    try {
+        result = await DB.Settings.list()
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
 
-router.post('/addSetting', async (req, res) => {})
-router.post('/deleteSetting', async (req, res) => {})
-router.post('/updateSetting', async (req, res) => {})
+router.get('/getTagTypes', async (req, res) => {
+    try {
+        result = await DB.Tag.listTagTypes()
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
 
-router.post('/addTagType', async (req, res) => {})
-router.post('/deleteTagType', async (req, res) => {})
-router.post('/updateTagType', async (req, res) => {})
+router.get('/getTrains', async (req, res) => {
+    try {
+        result = await DB.Train.getTrainRecord()
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.get('/getTrainExcludes', async (req, res) => {
+    try {
+        result = await DB.Train.getTrainExcluded(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
 
-router.post('/deleteTrain', async (req, res) => {})
+router.post('/addAuthor', async (req, res) => {
+    try {
+        result = await DB.Author.add(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/blockAuthor', async (req, res) => {
+    try {
+        result = await DB.Author.block(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/unblockAuthor', async (req, res) => {
+    try {
+        result = await DB.Author.unblock(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
 
-router.post('/addTrainExcludes', async (req, res) => {})
-router.post('/deleteTrainExcludes', async (req, res) => {})
-router.post('/updateTrainExcludes', async (req, res) => {})
+router.post('/addSetting', async (req, res) => {
+    try {
+        result = await DB.Settings.add(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/deleteSetting', async (req, res) => {
+    try {
+        result = await DB.Settings.deleteS(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/updateSetting', async (req, res) => {
+    try {
+        result = await DB.Settings.update(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+
+router.post('/addTagType', async (req, res) => {
+    try {
+        result = await DB.Tag.addTagType(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+
+router.post('/addTrainExcludes', async (req, res) => {
+    try {
+        result = await DB.Train.addTrainExcluded(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/deleteTrainExcludes', async (req, res) => {
+    try {
+        result = await DB.Train.deleteTrainExcluded(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/updateTrainExcludes', async (req, res) => {
+    try {
+        result = await DB.Train.updateTrainExcluded(req.body)
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
 
 module.exports = router
