dcb5c92cdad3a9cf9c27eae8e5dc0d71b833e925
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-06 12:52:20 +0300

    Dashboard route added

--- database/index.js
+++ database/index.js
@@ -5,6 +5,7 @@ const Word = require('./Word')
 const Settings = require('./Settings')
 const Tag = require('./Tag')
 const Train = require('./Train')
+const Dashboard = require('./Dashboard')
 
 module.exports = {
     User,
@@ -13,5 +14,6 @@ module.exports = {
     Word,
     Settings,
     Tag,
-    Train
+    Train,
+    Dashboard
 }
