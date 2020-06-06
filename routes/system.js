8390f4f5ac4e1743cdf1e5ff4a6ec6101fb1e58e
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-06 21:14:51 +0300

    Verify function added

--- routes/system.js
+++ routes/system.js
@@ -2,143 +2,223 @@ const router = require('express').Router()
 const DB = require('../database/index')
 const verify = require('../middleware/verify')
 
-router.get('/getUsers', async (req, res) => {
+router.get('/getUsers', verify, async (req, res) => {
     try {
-        data = {}
-        page = parseInt(req.query.page || 0)
-        data.offset = page * 15
-        result = await DB.User.list(data)
-        res.status(200).send(result)
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            data = {}
+            page = parseInt(req.query.page || 0)
+            data.offset = page * 15
+            result = await DB.User.list(data)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
     } catch (error) {
         res.status(400).send({ status: false, error: error })
     }
 })
-router.get('/getAuthors', async (req, res) => {
+router.get('/getAuthors', verify, async (req, res) => {
     try {
-        data = {}
-        page = parseInt(req.query.page || 0)
-        data.offset = page * 15
-        result = await DB.Author.list(data)
-        res.status(200).send(result)
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            data = {}
+            page = parseInt(req.query.page || 0)
+            data.offset = page * 15
+            result = await DB.Author.list(data)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
     } catch (error) {
         res.status(400).send({ status: false, error: error })
     }
 })
-
-router.get('/getSettings', async (req, res) => {
+router.get('/getSettings', verify, async (req, res) => {
     try {
-        result = await DB.Settings.list()
-        res.status(200).send(result)
-    } catch (error) {
-        res.status(400).send({ status: false, error: error })
-    }
-})
-
-router.get('/getTagTypes', async (req, res) => {
-    try {
-        result = await DB.Tag.listTagTypes()
-        res.status(200).send(result)
-    } catch (error) {
-        res.status(400).send({ status: false, error: error })
-    }
-})
-
-router.get('/getTrains', async (req, res) => {
-    try {
-        result = await DB.Train.getTrainRecord()
-        res.status(200).send(result)
-    } catch (error) {
-        res.status(400).send({ status: false, error: error })
-    }
-})
-router.get('/getTrainExcludes', async (req, res) => {
-    try {
-        result = await DB.Train.getTrainExcluded(req.body)
-        res.status(200).send(result)
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Settings.list()
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
     } catch (error) {
         res.status(400).send({ status: false, error: error })
     }
 })
 
-router.post('/addAuthor', async (req, res) => {
+router.get('/getTagTypes', verify, async (req, res) => {
     try {
-        result = await DB.Author.add(req.body)
-        res.status(200).send(result)
-    } catch (error) {
-        res.status(400).send({ status: false, error: error })
-    }
-})
-router.post('/blockAuthor', async (req, res) => {
-    try {
-        result = await DB.Author.block(req.body)
-        res.status(200).send(result)
-    } catch (error) {
-        res.status(400).send({ status: false, error: error })
-    }
-})
-router.post('/unblockAuthor', async (req, res) => {
-    try {
-        result = await DB.Author.unblock(req.body)
-        res.status(200).send(result)
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Tag.listTagTypes()
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
     } catch (error) {
         res.status(400).send({ status: false, error: error })
     }
 })
 
-router.post('/addSetting', async (req, res) => {
+router.get('/getTrains', verify, async (req, res) => {
     try {
-        result = await DB.Settings.add(req.body)
-        res.status(200).send(result)
-    } catch (error) {
-        res.status(400).send({ status: false, error: error })
-    }
-})
-router.post('/deleteSetting', async (req, res) => {
-    try {
-        result = await DB.Settings.deleteS(req.body)
-        res.status(200).send(result)
-    } catch (error) {
-        res.status(400).send({ status: false, error: error })
-    }
-})
-router.post('/updateSetting', async (req, res) => {
-    try {
-        result = await DB.Settings.update(req.body)
-        res.status(200).send(result)
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Train.getTrainRecord()
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
     } catch (error) {
         res.status(400).send({ status: false, error: error })
     }
 })
 
-router.post('/addTagType', async (req, res) => {
+router.get('/getTrainExcludes', verify, async (req, res) => {
     try {
-        result = await DB.Tag.addTagType(req.body)
-        res.status(200).send(result)
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Train.getTrainExcluded(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
     } catch (error) {
         res.status(400).send({ status: false, error: error })
     }
 })
 
-router.post('/addTrainExcludes', async (req, res) => {
+router.post('/addAuthor', verify, async (req, res) => {
     try {
-        result = await DB.Train.addTrainExcluded(req.body)
-        res.status(200).send(result)
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Author.add(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
     } catch (error) {
         res.status(400).send({ status: false, error: error })
     }
 })
-router.post('/deleteTrainExcludes', async (req, res) => {
+router.post('/blockAuthor', verify, async (req, res) => {
     try {
-        result = await DB.Train.deleteTrainExcluded(req.body)
-        res.status(200).send(result)
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Author.block(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
     } catch (error) {
         res.status(400).send({ status: false, error: error })
     }
 })
-router.post('/updateTrainExcludes', async (req, res) => {
+router.post('/unblockAuthor', verify, async (req, res) => {
     try {
-        result = await DB.Train.updateTrainExcluded(req.body)
-        res.status(200).send(result)
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Author.unblock(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+
+router.post('/addSetting', verify, async (req, res) => {
+    try {
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Settings.add(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/deleteSetting', verify, async (req, res) => {
+    try {
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Settings.deleteS(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/updateSetting', verify, async (req, res) => {
+    try {
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Settings.update(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+
+router.post('/addTagType', verify, async (req, res) => {
+    try {
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Tag.addTagType(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+
+router.post('/addTrainExcludes', verify, async (req, res) => {
+    try {
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Train.addTrainExcluded(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/deleteTrainExcludes', verify, async (req, res) => {
+    try {
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Train.deleteTrainExcluded(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
+    } catch (error) {
+        res.status(400).send({ status: false, error: error })
+    }
+})
+router.post('/updateTrainExcludes', verify, async (req, res) => {
+    try {
+        isUserAdmin = await DB.User.isUserAdmin({ id: req.userid.id })
+        if (isUserAdmin.status === true) {
+            result = await DB.Train.updateTrainExcluded(req.body)
+            res.status(200).send(result)
+        } else {
+            res.status(401).send('Only for admin')
+        }
     } catch (error) {
         res.status(400).send({ status: false, error: error })
     }
