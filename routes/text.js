9435618b9608df0fedf7d8201204c3049ad8eded
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-06 12:55:32 +0300

    Naming errors are fixed

--- routes/text.js
+++ routes/text.js
@@ -7,21 +7,19 @@ var path = require('path')
 const { server } = require('../ner/index')
 
 router.post('/add', verify, async (req, res) => {
-    console.log(req.id)
     try {
         let filename = path.join(
             __dirname,
-            '../texts/' + req.userid + '/' + moment().utc().format('Y-M-D-H:M:S') + '.txt'
+            '../texts/' + req.userid.id + '/' + moment().utc().format('Y-M-D-H:M:S') + '.txt'
         )
         fs.outputFileSync(filename, req.body.content)
 
         text = await DB.Text.add({
             path: filename,
-            userID: req.id,
+            userID: req.userid.id,
             type: 1
         })
         textid = text.id
-        console.log(textid)
         await server.post(req.body.content, async (err, resS) => {
             var tags = []
             for (let i = 0; i < resS.tags.length; i++) {
@@ -67,6 +65,7 @@ router.get('/:textid', verify, async (req, res) => {
 router.post('/:textid', verify, async (req, res) => {
     try {
         data = {}
+        data.textid = req.params.textid
         data.userID = req.userid.id
         data.tags = req.body.tags
         result = await DB.Tag.addRecord(data)
