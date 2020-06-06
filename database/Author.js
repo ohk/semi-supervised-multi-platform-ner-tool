7a84bb76072f529d6dbea72574f9b0b9af509ca4
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-06 19:50:23 +0300

    new functions added

--- database/Author.js
+++ database/Author.js
@@ -1,10 +1,24 @@
 const pool = require('./pool')
 
+list = async (data) => {
+    try {
+        var conneciton = pool.getPool()
+        const query = {
+            text: 'SELECT * FROM author ORDER BY authorid ASC LIMIT 15 OFFSET $1',
+            values: [data.offset]
+        }
+        authors = await conneciton.query(query)
+        return { status: true, data: authors.rows }
+    } catch (error) {
+        return { status: false, data: [] }
+    }
+}
+
 get = async () => {
     try {
         var conneciton = pool.getPool()
         const query = {
-            text: 'SELECT * FROM author'
+            text: 'SELECT * FROM author WHERE crawl = true'
         }
         authors = await conneciton.query(query)
         return { status: true, data: authors.rows }
@@ -40,7 +54,46 @@ add = async (data) => {
     }
 }
 
+block = async (data) => {
+    try {
+        var conneciton = pool.getPool()
+        const blockQ = {
+            text: 'UPDATE author SET crawl = false WHERE authorid= ($1)',
+            values: [data.authorid]
+        }
+        block = await conneciton.query(blockQ)
+        if (block.rowCount != 0) {
+            return { status: true, message: 'Author successfully added' }
+        } else {
+            return { status: false, message: 'Query error, please try again' }
+        }
+    } catch (error) {
+        return { status: false, message: error.message }
+    }
+}
+
+unblock = async (data) => {
+    try {
+        var conneciton = pool.getPool()
+        const blockQ = {
+            text: 'UPDATE author SET crawl = true WHERE authorid= ($1)',
+            values: [data.authorid]
+        }
+        block = await conneciton.query(blockQ)
+        if (block.rowCount != 0) {
+            return { status: true, message: 'Author successfully added' }
+        } else {
+            return { status: false, message: 'Query error, please try again' }
+        }
+    } catch (error) {
+        return { status: false, message: error.message }
+    }
+}
+
 module.exports = {
+    list,
+    add,
     get,
-    add
+    block,
+    unblock
 }
