67d5d1318ff6cfd898bb7c6bef8e8a944a7f6ed1
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-27 22:17:41 +0300

    Footer Added

--- web/src/layouts/Minimal/Minimal.js
+++ web/src/layouts/Minimal/Minimal.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/styles'
 
-import { Topbar } from './components';
+import { Topbar, Footer } from './components'
 
 const useStyles = makeStyles(() => ({
-  root: {
-    paddingTop: 64,
-    height: '100%'
-  },
-  content: {
-    height: '100%'
-  }
-}));
+    root: {
+        paddingTop: 64,
+        height: '100%'
+    },
+    content: {
+        height: '90%'
+    }
+}))
 
 const Minimal = props => {
-  const { children } = props;
+    const { children } = props
 
-  const classes = useStyles();
+    const classes = useStyles()
 
-  return (
-    <div className={classes.root}>
-      <Topbar />
-      <main className={classes.content}>{children}</main>
-    </div>
-  );
-};
+    return (
+        <div className={classes.root}>
+            <Topbar />
+            <main className={classes.content}>{children}</main>
+            <Footer />
+        </div>
+    )
+}
 
 Minimal.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string
-};
+    children: PropTypes.node,
+    className: PropTypes.string
+}
 
-export default Minimal;
+export default Minimal
