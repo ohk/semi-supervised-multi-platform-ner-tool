d4662585dcc86c0fac34092f3c38f39f9d0fe294
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-27 22:17:08 +0300

    Navigation redesigned

--- web/src/layouts/Main/components/Sidebar/Sidebar.js
+++ web/src/layouts/Main/components/Sidebar/Sidebar.js
@@ -1,119 +1,103 @@
-import React from 'react';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import { Divider, Drawer } from '@material-ui/core';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import PeopleIcon from '@material-ui/icons/People';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-import TextFieldsIcon from '@material-ui/icons/TextFields';
-import ImageIcon from '@material-ui/icons/Image';
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import SettingsIcon from '@material-ui/icons/Settings';
-import LockOpenIcon from '@material-ui/icons/LockOpen';
-
-import { Profile, SidebarNav, UpgradePlan } from './components';
+import React from 'react'
+import clsx from 'clsx'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/styles'
+import { Drawer } from '@material-ui/core'
+import DashboardIcon from '@material-ui/icons/Dashboard'
+import PeopleIcon from '@material-ui/icons/People'
+import TextFieldsIcon from '@material-ui/icons/TextFields'
+import SettingsIcon from '@material-ui/icons/Settings'
+import CreateIcon from '@material-ui/icons/Create'
+import LabelIcon from '@material-ui/icons/Label'
+import { SidebarNav } from './components'
+import AutorenewIcon from '@material-ui/icons/Autorenew'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 
 const useStyles = makeStyles(theme => ({
-  drawer: {
-    width: 240,
-    [theme.breakpoints.up('lg')]: {
-      marginTop: 64,
-      height: 'calc(100% - 64px)'
+    drawer: {
+        width: 240,
+        [theme.breakpoints.up('lg')]: {
+            marginTop: 64,
+            height: 'calc(100% - 64px)'
+        }
+    },
+    root: {
+        backgroundColor: theme.palette.white,
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        padding: theme.spacing(2)
+    },
+    divider: {
+        margin: theme.spacing(2, 0)
+    },
+    nav: {
+        marginBottom: theme.spacing(2)
     }
-  },
-  root: {
-    backgroundColor: theme.palette.white,
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100%',
-    padding: theme.spacing(2)
-  },
-  divider: {
-    margin: theme.spacing(2, 0)
-  },
-  nav: {
-    marginBottom: theme.spacing(2)
-  }
-}));
+}))
 
 const Sidebar = props => {
-  const { open, variant, onClose, className, ...rest } = props;
+    const { open, variant, onClose, className, ...rest } = props
 
-  const classes = useStyles();
+    const classes = useStyles()
 
-  const pages = [
-    {
-      title: 'Dashboard',
-      href: '/dashboard',
-      icon: <DashboardIcon />
-    },
-    {
-      title: 'Users',
-      href: '/users',
-      icon: <PeopleIcon />
-    },
-    {
-      title: 'Products',
-      href: '/products',
-      icon: <ShoppingBasketIcon />
-    },
-    {
-      title: 'Authentication',
-      href: '/sign-in',
-      icon: <LockOpenIcon />
-    },
-    {
-      title: 'Typography',
-      href: '/typography',
-      icon: <TextFieldsIcon />
-    },
-    {
-      title: 'Icons',
-      href: '/icons',
-      icon: <ImageIcon />
-    },
-    {
-      title: 'Account',
-      href: '/account',
-      icon: <AccountBoxIcon />
-    },
-    {
-      title: 'Settings',
-      href: '/settings',
-      icon: <SettingsIcon />
-    }
-  ];
+    const pages = [
+        {
+            title: 'Dashboard',
+            href: '/dashboard',
+            icon: <DashboardIcon />
+        },
+        {
+            title: 'Users',
+            href: '/users',
+            icon: <PeopleIcon />
+        },
+        {
+            title: 'Authors',
+            href: '/authors',
+            icon: <CreateIcon />
+        },
+        {
+            title: 'Tag Types',
+            href: '/tagtypes',
+            icon: <LabelIcon />
+        },
+        {
+            title: 'Trains',
+            href: '/trains',
+            icon: <AutorenewIcon />
+        },
+        {
+            title: 'Texts',
+            href: '/texts',
+            icon: <TextFieldsIcon />
+        },
+        {
+            title: 'Settings',
+            href: '/settings',
+            icon: <SettingsIcon />
+        },
+        {
+            title: 'Logout',
+            href: '/',
+            icon: <ExitToAppIcon />
+        }
+    ]
 
-  return (
-    <Drawer
-      anchor="left"
-      classes={{ paper: classes.drawer }}
-      onClose={onClose}
-      open={open}
-      variant={variant}
-    >
-      <div
-        {...rest}
-        className={clsx(classes.root, className)}
-      >
-        <Profile />
-        <Divider className={classes.divider} />
-        <SidebarNav
-          className={classes.nav}
-          pages={pages}
-        />
-        <UpgradePlan />
-      </div>
-    </Drawer>
-  );
-};
+    return (
+        <Drawer anchor="left" classes={{ paper: classes.drawer }} onClose={onClose} open={open} variant={variant}>
+            <div {...rest} className={clsx(classes.root, className)}>
+                <SidebarNav className={classes.nav} pages={pages} />
+            </div>
+        </Drawer>
+    )
+}
 
 Sidebar.propTypes = {
-  className: PropTypes.string,
-  onClose: PropTypes.func,
-  open: PropTypes.bool.isRequired,
-  variant: PropTypes.string.isRequired
-};
+    className: PropTypes.string,
+    onClose: PropTypes.func,
+    open: PropTypes.bool.isRequired,
+    variant: PropTypes.string.isRequired
+}
 
-export default Sidebar;
+export default Sidebar
