940d5bb7eb63daeb55e279829ddac8a28cf664b9
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-27 22:17:28 +0300

    Navigation updated

--- web/src/layouts/Main/components/Sidebar/components/SidebarNav/SidebarNav.js
+++ web/src/layouts/Main/components/Sidebar/components/SidebarNav/SidebarNav.js
@@ -1,96 +1,93 @@
 /* eslint-disable react/no-multi-comp */
 /* eslint-disable react/display-name */
-import React, { forwardRef } from 'react';
-import { NavLink as RouterLink } from 'react-router-dom';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import { List, ListItem, Button, colors } from '@material-ui/core';
+import React, { forwardRef } from 'react'
+import { NavLink as RouterLink } from 'react-router-dom'
+import clsx from 'clsx'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/styles'
+import { List, ListItem, Button, colors } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
-  root: {},
-  item: {
-    display: 'flex',
-    paddingTop: 0,
-    paddingBottom: 0
-  },
-  button: {
-    color: colors.blueGrey[800],
-    padding: '10px 8px',
-    justifyContent: 'flex-start',
-    textTransform: 'none',
-    letterSpacing: 0,
-    width: '100%',
-    fontWeight: theme.typography.fontWeightMedium
-  },
-  icon: {
-    color: theme.palette.icon,
-    width: 24,
-    height: 24,
-    display: 'flex',
-    alignItems: 'center',
-    marginRight: theme.spacing(1)
-  },
-  active: {
-    color: theme.palette.primary.main,
-    fontWeight: theme.typography.fontWeightMedium,
-    '& $icon': {
-      color: theme.palette.primary.main
+    root: {},
+    item: {
+        display: 'flex',
+        paddingTop: 0,
+        paddingBottom: 0
+    },
+    button: {
+        color: colors.blueGrey[800],
+        padding: '10px 8px',
+        justifyContent: 'flex-start',
+        textTransform: 'none',
+        letterSpacing: 0,
+        width: '100%',
+        fontWeight: theme.typography.fontWeightMedium
+    },
+    icon: {
+        color: theme.palette.icon,
+        width: 24,
+        height: 24,
+        display: 'flex',
+        alignItems: 'center',
+        marginRight: theme.spacing(1)
+    },
+    active: {
+        color: theme.palette.primary.main,
+        fontWeight: theme.typography.fontWeightMedium,
+        '& $icon': {
+            color: theme.palette.primary.main
+        }
     }
-  }
-}));
+}))
 
 const CustomRouterLink = forwardRef((props, ref) => (
-  <div ref={ref} style={{ flexGrow: 1 }}>
-    <RouterLink {...props} />
-  </div>
-));
+    <div ref={ref} style={{ flexGrow: 1 }}>
+        <RouterLink {...props} />
+    </div>
+))
 
 const SidebarNav = props => {
-  const { pages, className, ...rest } = props;
+    const { pages, className, ...rest } = props
 
-  const classes = useStyles();
+    const classes = useStyles()
 
-  return (
-    <List {...rest} className={clsx(classes.root, className)}>
-      {pages.map(page => (
-        <ListItem className={classes.item} disableGutters key={page.title}>
-          <Button
-            activeClassName={classes.active}
-            className={classes.button}
-            component={CustomRouterLink}
-            to={page.href}>
-            <div className={classes.icon}>{page.icon}</div>
-            {page.title}
-          </Button>
-        </ListItem>
-      ))}
-
-      <ListItem disableGutters key="Logout">
-        <Button
-          onClick={() => {
-            localStorage.clear();
-            window.location = './';
-          }}>
-          <div className={classes.icon}>
-            <svg
-              class="MuiSvgIcon-root"
-              focusable="false"
-              viewBox="0 0 24 24"
-              aria-hidden="true">
-              <path d="M21 3.01H3c-1.1 0-2 .9-2 2V9h2V4.99h18v14.03H3V15H1v4.01c0 1.1.9 1.98 2 1.98h18c1.1 0 2-.88 2-1.98v-14c0-1.11-.9-2-2-2zM11 16l4-4-4-4v3H1v2h10v3z"></path>
-            </svg>
-          </div>
-          Logout
-        </Button>
-      </ListItem>
-    </List>
-  );
-};
+    return (
+        <List {...rest} className={clsx(classes.root, className)}>
+            {pages.map(page => (
+                <ListItem className={classes.item} disableGutters key={page.title}>
+                    {page.title === 'Logout' ? (
+                        <Button
+                            activeClassName={classes.active}
+                            className={classes.button}
+                            component={CustomRouterLink}
+                            to={page.href}
+                            onClick={() => {
+                                localStorage.clear()
+                            }}
+                        >
+                            <div className={classes.icon}>{page.icon}</div>
+                            {page.title}
+                        </Button>
+                    ) : (
+                        <Button
+                            activeClassName={classes.active}
+                            className={classes.button}
+                            component={CustomRouterLink}
+                            to={page.href}
+                        >
+                            <div className={classes.icon}>{page.icon}</div>
+                            {page.title}
+                        </Button>
+                    )}
+                </ListItem>
+            ))}
+        </List>
+    )
+}
 
 SidebarNav.propTypes = {
-  className: PropTypes.string,
-  pages: PropTypes.array.isRequired
-};
+    className: PropTypes.string,
+    pages: PropTypes.array.isRequired
+}
 
-export default SidebarNav;
+export default SidebarNav
