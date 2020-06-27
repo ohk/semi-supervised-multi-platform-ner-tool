9bffb31368cdd60b48474f90c71e343c3f13bb74
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-06-28 02:20:43 +0300

    Routes updated

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -6,10 +6,7 @@ import { Main as MainLayout, Minimal as MinimalLayout } from './layouts'
 
 import {
     Dashboard as DashboardView,
-    ProductList as ProductListView,
     UserList as UserListView,
-    Typography as TypographyView,
-    Icons as IconsView,
     Account as AccountView,
     Settings as SettingsView,
     SignUp as SignUpView,
@@ -19,6 +16,8 @@ import {
     ForgotPassword as ForgotView,
     Text as TextView,
     Texts as TextsView,
+    AddText as addTextView,
+    Unauthorized as UnauthorizedView,
     NotFound as NotFoundView
 } from './views'
 
@@ -27,19 +26,17 @@ const Routes = () => {
         <Switch>
             <RouteWithLayout component={DashboardView} exact layout={MainLayout} path="/dashboard" />
             <RouteWithLayout component={UserListView} exact layout={MainLayout} path="/users" />
-            <RouteWithLayout component={ProductListView} exact layout={MainLayout} path="/products" />
-            <RouteWithLayout component={TypographyView} exact layout={MainLayout} path="/typography" />
-            <RouteWithLayout component={IconsView} exact layout={MainLayout} path="/icons" />
             <RouteWithLayout component={AccountView} exact layout={MainLayout} path="/account" />
             <RouteWithLayout component={SettingsView} exact layout={MainLayout} path="/settings" />
             <RouteWithLayout component={TextsView} exact layout={MainLayout} path="/text" />
             <RouteWithLayout component={TextView} exact layout={MainLayout} path="/text/*" />
-
+            <RouteWithLayout component={addTextView} exact layout={MainLayout} path="/addText" />
             <RouteWithLayout component={SignUpView} exact layout={MinimalLayout} path="/sign-up" />
             <RouteWithLayout component={SignInView} exact layout={MinimalLayout} path="/" />
             <RouteWithLayout component={ValidateView} exact layout={MinimalLayout} path="/validate/*" />
             <RouteWithLayout component={ResetView} exact layout={MinimalLayout} path="/reset/*" />
             <RouteWithLayout component={ForgotView} exact layout={MinimalLayout} path="/forgot" />
+            <RouteWithLayout component={UnauthorizedView} exact layout={MinimalLayout} path="/unauthorized" />
             <RouteWithLayout component={NotFoundView} exact layout={MinimalLayout} path="/not-found" />
             <Redirect to="/not-found" />
         </Switch>
