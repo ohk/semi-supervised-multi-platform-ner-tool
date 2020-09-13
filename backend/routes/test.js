edf00e127058d01adb705f67cc36777a853a3e80
Ömer Hamid Kamışlı <ohkamisli@Omers-MacBook-Pro.local>
2020-09-13 13:40:50 +0300

    test

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/', async (req, res) => {
-    return res.status(200).send('Hello Admin')
+    return res.status(200).send('Hello Admin\nTest Başarılı\n')
 })
 
 module.exports = router
