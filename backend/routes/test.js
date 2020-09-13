af8a9585f227f4b1f7093137695b058019fa0415
Ömer Hamid Kamışlı <ohkamisli@Omers-MacBook-Pro.local>
2020-09-13 13:04:42 +0300

    Hello Admin

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.get('/', verify, async (req, res) => {
+router.get('/', async (req, res) => {
     return res.status(200).send('Hello Admin')
 })
 
