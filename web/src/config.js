215abcf4e4156f1f3bc3306b18671be01757141a
Ömer Hamid Kamışlı <ohkamisli@Omers-MacBook-Pro.local>
2020-09-20 09:45:37 +0300

    .env problem solved

--- web/src/config.js
+++ web/src/config.js
@@ -1,7 +1,4 @@
-const dotenv = require('dotenv')
-dotenv.config()
-
 module.exports = global.config = {
-    API_ENDPOINT: process.env.API_ENDPOINT,
-    GOOGLE_RECAPTCHA_KEY: process.env.GOOGLE_RECAPTCHA_KEY
+    API_ENDPOINT: 'http://178.242.195.38:9080/api',
+    GOOGLE_RECAPTCHA_KEY: '6LdY_8wZAAAAAD-2mfFCDHmV2RJUeBG7PvtfWC8n'
 }
